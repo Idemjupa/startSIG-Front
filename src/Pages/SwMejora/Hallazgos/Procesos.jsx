a55479c6422fadb285058a4fdf42ed68916d9739
idemjupa <jupa_117@hotmail.com>
2023-09-22 00:40:18 -0500

    oops Hallazgos r

--- src/Pages/SwMejora/Hallazgos/Procesos.jsx
+++ src/Pages/SwMejora/Hallazgos/Procesos.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import  { useEffect, useState } from "react";
 import ProcesosModal from "./ProcesosModal";
 import DataTable from "react-data-table-component";
 import Swal from "sweetalert2";
